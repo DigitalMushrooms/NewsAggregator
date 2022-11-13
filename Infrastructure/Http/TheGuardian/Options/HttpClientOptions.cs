b735e16c1e020da1f7b27483663cae956eb7155c
Łukasz Grzybowski <lukasz.grzybowski.pl@gmail.com>
2022-11-13 12:57:19 +0100

    Revert change in HttpClientOptions. Properties should have set in parameters.

--- Infrastructure/Http/TheGuardian/Options/HttpClientOptions.cs
+++ Infrastructure/Http/TheGuardian/Options/HttpClientOptions.cs
@@ -3,6 +3,6 @@ namespace Infrastructure.Http.TheGuardian.Options;
 public class HttpClientOptions
 {
     public static string TheGuardianSection = "HttpClient:TheGuardian";
-    public string Url => string.Empty;
-    public string Key => string.Empty;
+    public string Url { get; set; } = string.Empty;
+    public string Key { get; set; } = string.Empty;
 }
