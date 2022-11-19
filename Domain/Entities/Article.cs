020b9e27619d0d7327e27c0703117f196adddc59
Łukasz Grzybowski <lukasz.grzybowski.pl@gmail.com>
2022-11-19 12:44:13 +0100

    Added Domain project with Article and BaseEntity types.

---
+++ Domain/Entities/Article.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Entities;
+
+public class Article : BaseEntity
+{
+    public PublisherName Publisher { get; set; }
+    public string Title { get; set; } = null!;
+    public DateTimeOffset PublicationDate { get; set; }
+}
+
+public enum PublisherName
+{
+    TheGuardian
+}
